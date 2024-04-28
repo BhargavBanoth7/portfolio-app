7369d1d373737f458903f233943eb5add8ffe7b9
BhargavBanoth7 <167991036+BhargavBanoth7@users.noreply.github.com>
2024-04-28 19:00:46 +1000

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,17 +3,19 @@ import "../styles.css";
 import logo from "../images/logo.jpg"
 import { Link } from "react-router-dom";
 
-
+// Adding Navigation bar on the top of the application
 function Navbar() {
   return (
     <nav className="nav">
       <div className="logo">
 
+        {/* Adding Portofolio picture */}
         <img src={logo} style={{
           width: 22,height:22}} alt="Logo"/>
         Bhargav Banoth's Portfolio
       </div>
-      
+
+      {/* Adding nav bar options and linking to endpoints*/}
       <ul className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/resume">Resume</Link>
